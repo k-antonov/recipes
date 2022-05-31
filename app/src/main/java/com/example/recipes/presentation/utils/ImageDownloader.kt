1faf7a09729f77b88db73641c9a037dc0be81207
Konstantin Antonov <koantonov99@gmail.com>
2022-05-31 20:49:27 +0700

    Add shimmer to recycler view item placeholder

--- app/src/main/java/com/example/recipes/presentation/utils/ImageDownloader.kt
+++ app/src/main/java/com/example/recipes/presentation/utils/ImageDownloader.kt
@@ -2,12 +2,29 @@ package com.example.recipes.presentation.utils
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.facebook.shimmer.Shimmer
+import com.facebook.shimmer.ShimmerDrawable
 
 class ImageDownloader {
+
     companion object {
+        private val shimmer = Shimmer.AlphaHighlightBuilder()
+            .setDuration(1800)
+            .setBaseAlpha(1f)
+            .setHighlightAlpha(0.9f)
+            .setDirection(Shimmer.Direction.LEFT_TO_RIGHT)
+            .build()
+
+        private val shimmerDrawable = ShimmerDrawable().apply {
+            setShimmer(shimmer)
+        }
+
         fun load(imageView: ImageView, imageUrl: String) {
             Glide.with(imageView.context)
                 .load(imageUrl)
+                .placeholder(shimmerDrawable)
+                .transition(DrawableTransitionOptions.withCrossFade())
                 .centerCrop()
                 .into(imageView)
         }
